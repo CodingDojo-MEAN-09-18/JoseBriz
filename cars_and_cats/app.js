e23c59ddd8eff64946053bff53562019c6d0d4a0
Jose Briz <brizjosem@gmail.com>
2018-09-13 13:39:44 -0500

    node cars and cats assignment

--- cars_and_cats/app.js
+++ cars_and_cats/app.js
@@ -1,11 +1,14 @@
-// get the http module:
-var http = require('http');
-// fs module allows us to read and write content for responses!!
-var fs = require('fs');
-// creating a server using http module:
+// include http, fs and url module
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
+    imageDir = '/images';
+
+// create server listening on port 7077
 var server = http.createServer(function (request, response){
-    // see what URL the clients are requesting:
-    console.log('client request URL: ', request.url);
+    
+    
     // this is how we do routing:
     if(request.url === '/') {
         fs.readFile('carscats.html', 'utf8', function (errors, contents){
@@ -36,7 +39,7 @@ var server = http.createServer(function (request, response){
         });
     }
     // for own .css files (not bootstrap on a cdn, this does not need configuartion)
-    else if(request.url === '/stylesheets/style.css') {   
+    else if(request.url.endsWith('/stylesheets/style.css')) {   
         fs.readFile('./stylesheets/style.css', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/css'});
             response.write(contents); 
@@ -44,7 +47,7 @@ var server = http.createServer(function (request, response){
         });
     }
     // for own .js files (not jquery on a cdn)
-    else if(request.url === '/scripts/script.js') {  
+    else if(request.url.endsWith('/scripts/script.js')) {  
         fs.readFile('./scripts/script.js', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/javascript'});
             response.write(contents); 
@@ -52,13 +55,21 @@ var server = http.createServer(function (request, response){
         });
     }
     // for image files
-    else if(request.url === '/images/home.jpg') {  
-        fs.readFile('./images/home.jpg', function (errors, contents){
-            response.writeHead(200, {'Content-Type': 'image/jpg'});
+    else if(request.url.startsWith('/images')) {
+        return fs.readFile('.'+request.url, function (errors, contents){
+            response.writeHead(200, {'Content-Type': ['image/jpg', 'image/jpeg']});
             response.write(contents); 
             response.end();
         });
     }
+    // instead of doing the below on image files because that would be one by one:
+    // else if(request.url === '/images/cat2.jpg') {  
+    //     fs.readFile('./images/cats/cat2.jpg', function (errors, contents){
+    //         response.writeHead(200, {'Content-Type': 'image/jpg'});
+    //         response.write(contents); 
+    //         response.end();
+    //     });
+    // }
     // request didn't match anything:
     else {
         fs.readFile('carscats.html', function (errors, contents){
